61d53dd429cb1c2d572b391754b92f8ca41abb72
MystxryXD <mystxryz@gmail.com>
2023-11-16 15:00:32 +0400

    shit

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ mod parser;
 mod interpreter;
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
-
     let code: String = env::args().nth(1).expect("Are you retarded?");
 
-    let mut vm = interpreter::Interpreter::new(code);
+    let mut vm: interpreter::Interpreter = interpreter::Interpreter::new(code);
     vm.run();
 }
